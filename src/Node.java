11483776228a60d9b3d70a0a4d1ee1cc428ecf96
dell <tanishka.bhardwaj_ccv17@gla.ac.in>
2019-02-20 11:21:28 +0530

    add constructor for class:Node (int,Node)

--- src/Node.java
+++ src/Node.java
@@ -22,7 +22,15 @@ public class Node {
 
     }
 
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
     public Node(int data) {
         this.data = data;
+
+
     }
+
 }
